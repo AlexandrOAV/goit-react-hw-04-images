4b51bcd65bfd3c14491cd7bb627fac87dad5c71e
AlexandrOAV <a0617011995@gmail.com>
2023-08-04 11:12:28 +0300

    page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import { Component } from "react";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Searchbar } from "./Searchbar/Searchbar";
 import {getImage} from 'recuest/recuestApi';
-
 import { Button } from './Button/Button';
 import { Loader } from './Searchbar/Loader/Loader';
 import { Modal } from './Modal/Modal';
