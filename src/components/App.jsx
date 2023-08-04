fa45d9d8ecdc6d401a903a6e694bd627e8e1e6ff
AlexandrOAV <a0617011995@gmail.com>
2023-08-04 20:51:07 +0300

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,24 +20,19 @@ export const App = () => {
   const [selectCart, setSelectCart] = useState(null);
   
 
-  // componentDidUpdate(_, prevState) {
-  //   const { textSerch, page } = this.state;
-  //   if (prevState.textSerch !== this.state.textSerch|| prevState.page !== page) {
-  //   this.addData(textSerch, page)
-  //   }
-  // }  
+  
   useEffect(() => {
+    if (textSerch === '') {
+      return
+    };
     addData(textSerch, page)
   }, [textSerch, page])
-  
   async function addData(textSearch, page) {
     try {
       setIsLoading(true);
       const newData = await getImage(textSearch, page);
-      console.log(newData)
-      // this.setState(prevState => (
-      //   { hits: page === 1 ? newData.hits : [...prevState.hits, ...newData.hits],
-      //    totalHits: newData.totalHits }));
+      setHits(prevHits => page === 1 ? newData.hits : [...prevHits, ...newData.hits]);
+      setTotalHits(newData.totalHits);
     } catch (error) {
       toast.error(`API NOT FAUND: ${error.message}`)
     } finally { setIsLoading(false) }
@@ -52,10 +47,8 @@ export const App = () => {
     
 
 
-  const clickInLoadMore = () => setPage(prevState => prevState.page + 1);
+  const clickInLoadMore = () => setPage(prevState => prevState + 1);
   
-  const openModal = () => setShowModal(true)
-
   const closeModal = () => setShowModal(false);
 
   const handleImageClick = (selectCart) => {
@@ -68,89 +61,24 @@ export const App = () => {
   return (
     <div>
       <ToastContainer position="top-center" autoClose={2000} />
-      <Searchbar onSubmit={this.hendleSerchSubmit} />
+      <Searchbar onSubmit={hendleSerchSubmit} />
       {isLoading && <Loader />}
       <ImageGallery
         hits={hits}
         alt={`image ${textSerch}`}
-        onClick={this.handleImageClick} />
-      {showButton && <Button totalHits={this.state.totalHits} onClick={this.clickInLoadMore} />}
-      {showModal && <Modal src={selectCart.largeImageURL} alt={`Big image ${textSerch}`} onClose={this.closeModal} />}
+        onClick={handleImageClick} />
+      {showButton &&
+        <Button
+          totalHits={totalHits}
+          onClick={clickInLoadMore} />}
+      {showModal &&
+        <Modal
+          src={selectCart.largeImageURL}
+          alt={`Big image ${textSerch}`}
+          onClose={closeModal}
+        />}
     </div>
   )
 }
 
 
-// export class App extends Component {
-// state = {
-//   textSerch: '',
-//   hits:[],
-//   page: 1,
-//   isLoading: false, 
-//   totalHits: 0,
-//   showModal: false,
-//   selectCart:null,
-//   }
-  
-//   componentDidUpdate(_, prevState) {
-//     const { textSerch, page } = this.state;
-//     if (prevState.textSerch !== this.state.textSerch|| prevState.page !== page) {
-//     this.addData(textSerch, page)
-//     }
-//   }  
-      
-  
-//   addData = async (textSearch, page) => {
-//     try {
-//     this.setState({isLoading:true})
-//     const newData = await getImage(textSearch, page);
-//       this.setState(prevState => (
-//         { hits: page === 1 ? newData.hits : [...prevState.hits, ...newData.hits],
-//          totalHits: newData.totalHits }));
-//     } catch (error) {
-//        toast.error(`API NOT FAUND: ${error.message}`)
-//     } finally { this.setState({isLoading:false})}
-   
-//   } 
-
-//   hendleSerchSubmit = (imageSerch) => {
-//     if (this.state.textSerch !== imageSerch) {
-//       this.setState({textSerch:imageSerch, page:1})
-//     }
-    
-//   }
-
-//   clickInLoadMore = () => {
-//     this.setState(prevState =>({page: prevState.page + 1}))
-//   }
-  
-//   openModal = () => {
-//    this.setState({showModal:true})
-//   }
-
-//   closeModal = () => {
-//     this.setState({showModal:false})
-//   }
-
-//   handleImageClick = (selectCart) => {
-//     this.setState({ selectCart, showModal:true });
-//   };
-
-//   render() {
-//     const { hits, textSerch, isLoading, totalHits, showModal, selectCart } = this.state;
-//     const showButton = hits.length > 0 && hits.length < totalHits;
-//        return (
-//          <div>
-//           <ToastContainer position="top-center" autoClose={2000}/>
-//            <Searchbar onSubmit={this.hendleSerchSubmit} />
-//            {isLoading&&<Loader/>}
-//            <ImageGallery
-//              hits={hits}
-//              alt={`image ${textSerch}`}
-//              onClick={this.handleImageClick} />
-//            {showButton && <Button totalHits={this.state.totalHits} onClick={this.clickInLoadMore} />}
-//            {showModal && <Modal src={selectCart.largeImageURL} alt={`Big image ${textSerch}`} onClose={this.closeModal}  />}
-//          </div>
-//   );
-//   }
-// };
